a1f530c908bb99238599bad341fc49ca5ec34ac9
ruku <hamaro1818@gmail.com>
2024-12-10 11:13:03 +0900

    fix: 静的サイトに変更.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,9 @@
 "use client";
 import { MdArrowForwardIos } from "react-icons/md";
 import { GiAchievement } from "react-icons/gi";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
-  const [data, setData] = useState([]);
-  const [canGet, setCanGet] = useState(true);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    try {
-      axios.get("/api/getRanking").then((res) => {
-        if (res.data.ok) {
-          setData(res.data.data);
-          setIsLoading(false);
-        } else {
-          setCanGet(false);
-        }
-      });
-    } catch {
-      setCanGet(false);
-    }
-    const interval = setInterval(() => {
-      try {
-        axios.get("/api/getRanking").then((res) => {
-          if (res.data.ok) {
-            setData(res.data.data);
-          } else {
-            setCanGet(false);
-          }
-        });
-      } catch {
-        setCanGet(false);
-      }
-    }, 30000);
-    return () => clearInterval(interval);
-  }, []);
   return (
     <div>
       <div className="h-10 bg-[#1c1c1c]"></div>
@@ -67,50 +34,44 @@ export default function Home() {
           </Link>
         </div>
         <div className="mt-10 text-center text-5xl font-bold text-[#222]">ストラックアウトランキング</div>
-        {isLoading ? (
-          <div className="text-2xl text-center">ロード中...</div>
-        ) : !canGet ? (
-          <div className="text-2xl text-center">取得できませんでした</div>
-        ) : (
-          <div className="flex justify-center mt-5 pb-10">
-            <div className="border-r border-black text-center text-2xl">
-              <div className="border-b-2 border-black px-7 py-2 flex">
-                <GiAchievement className="text-yellow-600" />
-                1位
-              </div>
-              <div className="border-b-2 border-black px-7 py-2 flex">
-                <GiAchievement className="text-zinc-400" />
-                2位
-              </div>
-              <div className="border-b-2 border-black px-7 py-2 flex">
-                <GiAchievement className="text-orange-700" />
-                3位
-              </div>
-              <div className="border-b-2 border-black px-7 py-2 flex">
-                <GiAchievement className="" />
-                4位
-              </div>
-              <div className="border-b-2 border-black px-7 py-2 flex">
-                <GiAchievement className="" />
-                5位
-              </div>
+        <div className="flex justify-center mt-5 pb-10">
+          <div className="border-r border-black text-center text-2xl">
+            <div className="border-b-2 border-black px-7 py-2 flex">
+              <GiAchievement className="text-yellow-600" />
+              1位
             </div>
-            <div className="border-l-r border-black text-2xl">
-              <div className="border-b-2 border-black px-7 py-2">{data[0]["username"]}</div>
-              <div className="border-b-2 border-black px-7 py-2">{data[1]["username"]}</div>
-              <div className="border-b-2 border-black px-7 py-2">{data[2]["username"]}</div>
-              <div className="border-b-2 border-black px-7 py-2">{data[3]["username"]}</div>
-              <div className="border-b-2 border-black px-7 py-2">{data[4]["username"]}</div>
+            <div className="border-b-2 border-black px-7 py-2 flex">
+              <GiAchievement className="text-zinc-400" />
+              2位
             </div>
-            <div className="border-l border-black text-2xl">
-              <div className="border-b-2 border-black px-7 py-2">{data[0]["score"]}点</div>
-              <div className="border-b-2 border-black px-7 py-2">{data[1]["score"]}点</div>
-              <div className="border-b-2 border-black px-7 py-2">{data[2]["score"]}点</div>
-              <div className="border-b-2 border-black px-7 py-2">{data[3]["score"]}点</div>
-              <div className="border-b-2 border-black px-7 py-2">{data[4]["score"]}点</div>
+            <div className="border-b-2 border-black px-7 py-2 flex">
+              <GiAchievement className="text-orange-700" />
+              3位
+            </div>
+            <div className="border-b-2 border-black px-7 py-2 flex">
+              <GiAchievement className="" />
+              4位
+            </div>
+            <div className="border-b-2 border-black px-7 py-2 flex">
+              <GiAchievement className="" />
+              5位
             </div>
           </div>
-        )}
+          <div className="border-l-r border-black text-2xl">
+            <div className="border-b-2 border-black px-7 py-2">アキラー（自由契約）</div>
+            <div className="border-b-2 border-black px-7 py-2">植草</div>
+            <div className="border-b-2 border-black px-7 py-2">しゅん</div>
+            <div className="border-b-2 border-black px-7 py-2">アキラー</div>
+            <div className="border-b-2 border-black px-7 py-2">村上頌樹(ノーコン)</div>
+          </div>
+          <div className="border-l border-black text-2xl">
+            <div className="border-b-2 border-black px-7 py-2">34点</div>
+            <div className="border-b-2 border-black px-7 py-2">30点</div>
+            <div className="border-b-2 border-black px-7 py-2">28点</div>
+            <div className="border-b-2 border-black px-7 py-2">28点</div>
+            <div className="border-b-2 border-black px-7 py-2">28点</div>
+          </div>
+        </div>
       </div>
     </div>
   );
